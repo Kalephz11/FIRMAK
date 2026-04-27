202cbfe45c3ae6539c85f268c4598ffe5464ae98
Kalephz11 <kaleph.zapata11@gmail.com>
2026-04-27 10:12:00 -0500

    fix ruta phpmailer lowercase

--- includes/mail.php
+++ includes/mail.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../PHPMailer/src/PHPMailer.php';
-require __DIR__ . '/../PHPMailer/src/SMTP.php';
-require __DIR__ . '/../PHPMailer/src/Exception.php';
+require __DIR__ . '/../phpmailer/src/PHPMailer.php';
+require __DIR__ . '/../phpmailer/src/SMTP.php';
+require __DIR__ . '/../phpmailer/src/Exception.php';
 
 function enviarCodigo($correo, $codigo) {
 
